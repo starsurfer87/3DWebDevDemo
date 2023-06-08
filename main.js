cc6b1e2a46c2a2db3e0b38376855085e2bcb6902
Skye <starsurfer2010@gmail.com>
2023-06-07 19:50:28 -0700

    Code  Cleanup

--- main.js
+++ main.js
@@ -7,144 +7,144 @@ add3D('models/simple_model_large.glb', '3D container', 0.5, 0.4);
 
 function add3D(filename, containerID, viewportSize, modelScale) {
 
-let camera, scene, renderer, controls;
+    let camera, scene, renderer, controls;
 
-const mixers = [];
+    const mixers = [];
 
-const clock = new THREE.Clock();
+    const clock = new THREE.Clock();
 
-const ASPECT_RATIO = 4/3;
+    const ASPECT_RATIO = 4 / 3;
 
-init();
-animate();
+    init();
+    animate();
 
-function init() {
-    const container = document.getElementById(containerID);
+    function init() {
+        const container = document.getElementById(containerID);
 
-    camera = new THREE.PerspectiveCamera( 30, ASPECT_RATIO, 1, 5000 );
-    camera.position.set( 0, 150, 500);
+        camera = new THREE.PerspectiveCamera(30, ASPECT_RATIO, 1, 5000);
+        camera.position.set(0, 150, 500);
 
-    scene = new THREE.Scene();
-    scene.background = new THREE.Color().setHSL(0,0,0.9);
-    scene.fog = new THREE.Fog( scene.background, 1, 5000 );
+        scene = new THREE.Scene();
+        scene.background = new THREE.Color().setHSL(0, 0, 0.9);
+        scene.fog = new THREE.Fog(scene.background, 1, 5000);
 
-    // HEMISPHERE LIGHT
+        // HEMISPHERE LIGHT
 
-    const hemiLight = new THREE.HemisphereLight( 0xffffff, 0xffffff, 0.6 );
-    hemiLight.color.setHSL( 0.6, 1, 0.6 );
-    hemiLight.groundColor.setHSL( 0.095, 1, 0.75 );
-    hemiLight.position.set( 0, 250, 0 );
-    scene.add( hemiLight );
+        const hemiLight = new THREE.HemisphereLight(0xffffff, 0xffffff, 0.6);
+        hemiLight.color.setHSL(0.6, 1, 0.6);
+        hemiLight.groundColor.setHSL(0.095, 1, 0.75);
+        hemiLight.position.set(0, 250, 0);
+        scene.add(hemiLight);
 
-    const hemiLightHelper = new THREE.HemisphereLightHelper( hemiLight, 10 );
-    scene.add( hemiLightHelper );
+        const hemiLightHelper = new THREE.HemisphereLightHelper(hemiLight, 10);
+        scene.add(hemiLightHelper);
 
-    // DIRECTIONAL LIGHT 
+        // DIRECTIONAL LIGHT 
 
-    const dirLight = new THREE.DirectionalLight( 0xffffff, 1 );
-    dirLight.color.setHSL( 0, 0, 1);
-    dirLight.position.set( - 1, 1.75, 1 );
-    dirLight.position.multiplyScalar( 150 );
-    scene.add( dirLight );
+        const dirLight = new THREE.DirectionalLight(0xffffff, 1);
+        dirLight.color.setHSL(0, 0, 1);
+        dirLight.position.set(- 1, 1.75, 1);
+        dirLight.position.multiplyScalar(150);
+        scene.add(dirLight);
 
-    dirLight.castShadow = true;
+        dirLight.castShadow = true;
 
-    dirLight.shadow.mapSize.width = 2048;
-    dirLight.shadow.mapSize.height = 2048;
+        dirLight.shadow.mapSize.width = 2048;
+        dirLight.shadow.mapSize.height = 2048;
 
-    const d = 500;
+        const d = 500;
 
-    dirLight.shadow.camera.left = - d;
-    dirLight.shadow.camera.right = d;
-    dirLight.shadow.camera.top = d;
-    dirLight.shadow.camera.bottom = - d;
+        dirLight.shadow.camera.left = - d;
+        dirLight.shadow.camera.right = d;
+        dirLight.shadow.camera.top = d;
+        dirLight.shadow.camera.bottom = - d;
 
-    dirLight.shadow.camera.far = 3500;
-    dirLight.shadow.bias = - 0.0001;
-    
-    // TODO: edit settings to suit final scene
-    // TODO: get shadows working
+        dirLight.shadow.camera.far = 3500;
+        dirLight.shadow.bias = - 0.0001;
 
-    const dirLightHelper = new THREE.DirectionalLightHelper( dirLight, 10 );
-    scene.add( dirLightHelper );
+        // TODO: edit settings to suit final scene
+        // TODO: get shadows working
 
-    // GROUND
+        const dirLightHelper = new THREE.DirectionalLightHelper(dirLight, 10);
+        scene.add(dirLightHelper);
 
-    const groundGeo = new THREE.PlaneGeometry( 10000, 10000 );
-    const groundMat = new THREE.MeshLambertMaterial( { color: 0xffffff } );
-    groundMat.color.setHSL( 0, 0, 0.75 );
+        // GROUND
 
-    const ground = new THREE.Mesh( groundGeo, groundMat );
-    ground.rotation.x = - Math.PI / 2;
-    ground.receiveShadow = true;
-    scene.add( ground );
+        const groundGeo = new THREE.PlaneGeometry(10000, 10000);
+        const groundMat = new THREE.MeshLambertMaterial({ color: 0xffffff });
+        groundMat.color.setHSL(0, 0, 0.75);
 
-    // MODEL
+        const ground = new THREE.Mesh(groundGeo, groundMat);
+        ground.rotation.x = - Math.PI / 2;
+        ground.receiveShadow = true;
+        scene.add(ground);
 
-    const loader = new GLTFLoader();
+        // MODEL
 
-    loader.load(filename, function ( gltf ) {
+        const loader = new GLTFLoader();
 
-        const mesh = gltf.scene;
+        loader.load(filename, function (gltf) {
 
-        mesh.scale.set( modelScale, modelScale, modelScale );
+            const mesh = gltf.scene;
 
-        mesh.castShadow = true;
-        mesh.receiveShadow = true;
+            mesh.scale.set(modelScale, modelScale, modelScale);
 
-        scene.add( mesh );
+            mesh.castShadow = true;
+            mesh.receiveShadow = true;
 
-        const mixer = new THREE.AnimationMixer( mesh );
-        mixer.clipAction( gltf.animations[ 0 ] ).play();
-        mixers.push( mixer );
+            scene.add(mesh);
 
-    } );
+            const mixer = new THREE.AnimationMixer(mesh);
+            mixer.clipAction(gltf.animations[0]).play();
+            mixers.push(mixer);
 
-    // RENDERER
+        });
 
-    renderer = new THREE.WebGLRenderer( { antialias: true } );
-    renderer.setPixelRatio( window.devicePixelRatio );
-    updateRendererSize();
-    container.appendChild( renderer.domElement );
-    renderer.shadowMap.enabled = true;
+        // RENDERER
 
-    // INTERACTIONS
+        renderer = new THREE.WebGLRenderer({ antialias: true });
+        renderer.setPixelRatio(window.devicePixelRatio);
+        updateRendererSize();
+        container.appendChild(renderer.domElement);
+        renderer.shadowMap.enabled = true;
 
-    window.addEventListener( 'resize', updateRendererSize );
+        // INTERACTIONS
 
-    controls = new OrbitControls(camera, renderer.domElement);
-    controls.target.set(0, 100 ,0);
-    controls.update();
-    controls.enablePan = false;
-    controls.maxPolarAngle = Math.PI/2;
-    // TODO: add in max/min values for zoom 
-    // (may also need to edit max values for rotation)
+        window.addEventListener('resize', updateRendererSize);
 
-}
-
-function updateRendererSize() {
-    renderer.setSize(window.innerWidth * viewportSize, window.innerWidth * viewportSize / ASPECT_RATIO);
-}
-
-function animate() {
-
-    requestAnimationFrame( animate );
-
-    render();
-
-}
-
-function render() {
-
-    const delta = clock.getDelta();
-
-    for ( let i = 0; i < mixers.length; i ++ ) {
-
-    mixers[ i ].update( delta );
+        controls = new OrbitControls(camera, renderer.domElement);
+        controls.target.set(0, 100, 0);
+        controls.update();
+        controls.enablePan = false;
+        controls.maxPolarAngle = Math.PI / 2;
+        // TODO: add in max/min values for zoom 
+        // (may also need to edit max values for rotation)
 
     }
 
-    renderer.render( scene, camera );
+    function updateRendererSize() {
+        renderer.setSize(window.innerWidth * viewportSize, window.innerWidth * viewportSize / ASPECT_RATIO);
+    }
 
-}
+    function animate() {
+
+        requestAnimationFrame(animate);
+
+        render();
+
+    }
+
+    function render() {
+
+        const delta = clock.getDelta();
+
+        for (let i = 0; i < mixers.length; i++) {
+
+            mixers[i].update(delta);
+
+        }
+
+        renderer.render(scene, camera);
+
+    }
 }
