023f8363d5d08a233a1210fcd8eb59633aa1a872
Skye <starsurfer2010@gmail.com>
2023-06-07 23:24:47 -0700

    Additions to HTML

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
 import {add3D} from './3Dcomponent.js';
 
-add3D('models/FinalSquat.gltf', '3D container', 0.95, 1);
-add3D('models/simple_model_large.glb', '3D container', 0.5, 0.4);
+add3D('models/FinalSquat.gltf', 'container1', 0.95, 1);
+add3D('models/simple_model_large.glb', 'container2', 0.5, 0.4);
